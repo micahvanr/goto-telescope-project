ac038f61dbe4a5cff5b8a9098815a0be36ae62a6
Micah VanRavenswaay <micahvanr02@gmail.com>
2026-01-27 19:32:56 -0600

    modify(ci): Modify makefile to output errors when a unit test fails
    The CI system will be able to catch the fail and ensure the branch cannot be merged until all tests pass

--- test/test_sample_avg.c
+++ test/test_sample_avg.c
@@ -10,13 +10,20 @@ void tearDown(void) {
 }
 
 void test_sample_avg(void) {
-    TEST_FAIL_MESSAGE("TEST");
+    float array[3] = {5, 5, 5,};
+    float avg = average(array, 3);
+    TEST_ASSERT_EQUAL_FLOAT(avg, 5);
+}
+
+void test_fail(void) {
+    TEST_FAIL_MESSAGE("Testing fail test");
 }
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_sample_avg);
+    //RUN_TEST(test_fail);
 
     return UNITY_END();
 }
